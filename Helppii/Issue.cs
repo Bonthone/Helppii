19a4e99f842d851236bb17c222ea952a58a0ae14
Miksu <mikael.koponen@gmail.com>
2012-02-16 21:24:43 +0200

    Add rest

---
+++ Helppii/Issue.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Device.Location;
+using System.ComponentModel;
+
+namespace helppii
+{
+    public class Issue : INotifyPropertyChanged
+    {
+        public Issue() { ; }
+        private GeoCoordinate _location;
+        private string _title;
+        private string _description;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged("Title");
+                }
+            }
+        }
+        public GeoCoordinate Location
+        {
+            get { return _location; }
+            set
+            {
+                if (_location != value)
+                {
+                    _location = value;
+                    OnPropertyChanged("Location");
+                }
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string Test()
+        {
+            return "test";
+        }
+    }
+}
