8a9199c92a23898efd77eba4205d1b5e5f8c7d43
unknown <Miksu@.(none)>
2012-02-16 17:08:13 +0200

    Ignore files

---
+++ Helppii/MapView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Device.Location;
+
+namespace helppii
+{
+    public partial class MapView : PhoneApplicationPage
+    {
+        GeoCoordinateWatcher watcher;
+
+        public MapView()
+        {
+            InitializeComponent();
+
+            watcher = new GeoCoordinateWatcher();
+            watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+            watcher.MovementThreshold = 25;
+            watcher.Start();
+        }
+
+
+        void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            myMap.Center = e.Position.Location;
+            myMap.ZoomLevel = 10;
+        }
+
+        private void Show_Zoom(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/View/SecondPage.xaml", UriKind.Relative));
+        }
+
+
+    }
+}
---
+++ Helppii/Page1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace helppii
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        GeoCoordinateWatcher watcher;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            watcher = new GeoCoordinateWatcher();
+            watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+            watcher.MovementThreshold = 25;
+            watcher.Start();
+        }
+
+
+        void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            myMap.Center = e.Position.Location;
+            myMap.ZoomLevel = 10;
+        }
+
+        private void Show_Zoom(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
